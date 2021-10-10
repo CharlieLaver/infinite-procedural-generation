e6154dd2b6cfca2844a11f90b502c27b17aac7f4
Charlie Laver <charlielaver7@gmail.com>
2021-10-10 17:26:27 +0100

    npc base class updates

---
+++ Assets/core/npc.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class npc : MonoBehaviour {
+    
+    private Vector3 pos;
+
+    protected void move(Transform target, float howClose, float speed) {
+        float distToTarget = Vector3.Distance(transform.position, transform.position);
+        float distToPos = Vector3.Distance(this.pos, transform.position);
+        if(distToPos < 5) {
+            randomPos();
+        }
+        if(distToTarget <= howClose) {
+            this.pos = target.position;
+        }
+        transform.LookAt(this.pos);
+        transform.position = Vector3.MoveTowards(transform.position, this.pos, speed * Time.deltaTime); 
+    }   
+
+    protected void randomPos() {
+        float x = Random.Range((transform.position.x - 30), (transform.position.x + 30));
+        float z = Random.Range((transform.position.z - 30), (transform.position.z + 30));
+        this.pos = new Vector3(x, 5, z);
+    } 
+
+    protected float damageCalculator(Collision collision, float health) {
+        if (collision.relativeVelocity.magnitude > 100) {
+            health -= 50;
+        }
+        else if (collision.relativeVelocity.magnitude > 60) {
+            health -= 30;
+        }
+        else if (collision.relativeVelocity.magnitude > 40) {
+            health -= 20;
+        }
+        else if (collision.relativeVelocity.magnitude > 20) {
+            health -= 10;
+        }
+        return health;
+    }
+
+    protected void die(int dropAmount, GameObject itemToDrop) {
+        Destroy(gameObject);
+        for (int i = 0; i < dropAmount; i++) {
+            Instantiate(itemToDrop, transform.position, transform.rotation);
+        }
+    }
+
+	protected void selectCharacteristics(string npcType) {
+		string[] meshResources = Directory.GetFiles(Application.dataPath + "/Resources/npc/" + npcType + "/meshes");
+		string randomMesh = Path.GetFileName(meshResources[Random.Range(0, meshResources.Length)]);
+		Mesh selectedMesh = Resources.Load<Mesh>("npc/" + npcType + "/meshes/" + randomMesh);
+		gameObject.GetComponent<MeshFilter>().mesh = selectedMesh;
+        gameObject.GetComponent<MeshCollider>().sharedMesh = selectedMesh;
+		string[] matResources = Directory.GetFiles(Application.dataPath + "/Resources/npc" + npcType + "/materials");
+		string randomMat = Path.GetFileName(matResources[Random.Range(0, matResources.Length)]);
+		Material selectedMat = Resources.Load<Material>("npc/" + npcType + "/materials/" + randomMat);
+		gameObject.GetComponent<Renderer>().material = selectedMat;
+	}
+
+}
--- Assets/surface/npcCollect.cs
+++ Assets/surface/npcCollect.cs
@@ -6,19 +6,23 @@ class npcCollect : npc {
 
     private float health = 100;
     private GameObject itemToDrop;
+	private Transform target;
 
     private void Start() {
+		selectCharacteristics("npcCollect");
+		this.target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
         InvokeRepeating("randomPos", 2f, 5f);
     }
 
     private void Update() {
-        if(health < 0) {
-            die(100, itemToDrop);
+		move(this.target, 10, 5);
+        if(this.health < 0) {
+            die(100, this.itemToDrop);
         }
     }
 
     private void OnCollisionEnter(Collision collision) {
-        health = damageCalculator(collision, health);
+        health = damageCalculator(collision, this.health);
     }
 
 }
