a4dbb9f32c46a8017717c4a22c82f2b5b2017918
Charlie Laver <charlielaver7@gmail.com>
2021-10-17 17:16:17 +0100

    weapon fire rate

--- Assets/Core/weapon.cs
+++ Assets/Core/weapon.cs
@@ -6,22 +6,28 @@ using System.IO;
 public class weapon : MonoBehaviour {
     
     protected Transform firePos;
+    private bool canShoot = true;
 
     protected void getFirePos() {
         firePos = GameObject.Find("firePos").transform;
     }
 
-    protected void shoot(string weaponName, float weaponPower) {
-        if(Input.GetMouseButtonDown(0)) {
-            if(inventory.projectiles > 0) {
-                string[] projectileResources = helpers.filterMetaFiles(Directory.GetFiles(Application.dataPath + "/Resources/inventory/projectiles/" + weaponName));
-                string randomProjectile = Path.GetFileNameWithoutExtension(projectileResources[Random.Range(0, projectileResources.Length)]);
-                GameObject projectile = Resources.Load<GameObject>("inventory/projectiles/" + weaponName + "/" + randomProjectile);
-                GameObject shot = Instantiate(projectile, firePos.position, firePos.rotation);
-                shot.GetComponent<Rigidbody>().AddRelativeForce(Vector3.forward * weaponPower);
-                inventory.projectiles--;
-            }
+    protected void shoot(string weaponName, float weaponPower, float shootDelay) {
+        if(Input.GetMouseButton(0) && this.canShoot && inventory.projectiles > 0) {
+            StartCoroutine(fireRate(weaponName, weaponPower, shootDelay));
         }
+    }
+
+    protected IEnumerator fireRate(string weaponName, float weaponPower, float shootDelay) {
+        string[] projectileResources = helpers.filterMetaFiles(Directory.GetFiles(Application.dataPath + "/Resources/inventory/projectiles/" + weaponName));
+        string randomProjectile = Path.GetFileNameWithoutExtension(projectileResources[Random.Range(0, projectileResources.Length)]);
+        GameObject projectile = Resources.Load<GameObject>("inventory/projectiles/" + weaponName + "/" + randomProjectile);
+        GameObject shot = Instantiate(projectile, firePos.position, firePos.rotation);
+        shot.GetComponent<Rigidbody>().AddRelativeForce(Vector3.forward * weaponPower);
+        inventory.projectiles--;        
+        this.canShoot = false;
+        yield return new WaitForSeconds(shootDelay);
+        this.canShoot = true;
     }
 
 }
--- Assets/Instances/weapon/shotgun.cs
+++ Assets/Instances/weapon/shotgun.cs
@@ -6,13 +6,14 @@ public class shotgun : weapon {
 
     private string weaponName = "shotgun";
     private float weaponPower = 4000;
+    private float fireRate = 1;
 
     private void Start() {
         getFirePos();
     }
 
     private void Update() {
-        shoot(weaponName, weaponPower);
+        shoot(weaponName, weaponPower, fireRate);
     }
-    
+
 }
